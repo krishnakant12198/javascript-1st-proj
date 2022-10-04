94e97990341faaa36d3a2be0a742e24667e11b6e
krishna kant <krishnakant12198@gmail.com>
2022-10-04 15:42:36 +0530

    javascript 1st proj

---
+++ index.js
@@ -0,0 +1,55 @@
+//arithmetic operator
+let a=200
+let b=100
+let add=a+b
+let sub=a-b
+let div=a/b
+let multi=a*b
+console.log("add " + add,"sub " + sub, "div "+div,"multi "+ multi)
+
+
+//relational 
+let a1=12, a2=60
+let rela= a2>a1
+let rela1= a1 != a2
+let rela2 = a2 != a1
+let rela3 = a1>a2
+console.log("a2>a1 "+rela,"a1 != "+ rela1, "a2!=a1 " +rela2,"a1>a2 "+ rela3)
+
+
+// logical 
+let b1= 100, b2=200
+let log1=(b1 && b2)
+let log2 = (b1 || b2)
+let log3 = (b1|b2)
+console.log(log1,log2,log3)
+
+
+//assignment
+let c1=20, c2=30, c3= 20
+let assi1= (c1==c2)
+let assi2 = (c1==c3)
+console.log(assi1)
+
+//decrement
+let d1=20, d2=30
+
+console.log(d1++, d1--, d2++, d2--)
+
+
+// Condition ?
+let E1=10, E2=20
+
+console.log(E1<E2)
+console.log(E2<E1) 
+
+// bitwise
+let F1=20, F2= 40;
+
+let bitwise1= F1 & F2;
+let bitwise2 = F1 | F2;
+let bitwise3 = F1 ^ F2
+
+console.log(bitwise1, bitwise2, bitwise3)
+
+
